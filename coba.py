231c3ea65cb6ec5210ded7f4079339baddf37041
adibharyo <70692957+adibharyo@users.noreply.github.com>
2022-10-20 17:11:02 +0700

    Add files via upload

--- coba.py
+++ coba.py
@@ -17,24 +17,55 @@ class UI(QMainWindow):
         open = gspread.service_account(filename="creds.json")
         sheets = open.open_by_key(
             "1gVmaW9uDWLIy7-B90HDruVZSlTZnTSGv_mGj-BmWRTo")
-        worksheets = sheets.worksheet("MODUL 8")
+        
+        tabWidget2 = self.findChild(QTableWidget, "Modul_7")
 
-        jadwal = worksheets.get_all_values()
-        for x in range(len(jadwal)):
-            self.insertRow(jadwal[x])
+        worksheetsSatu = sheets.worksheet("MODUL 6")
+        jadwalSatu = worksheetsSatu.get_all_values()
+        for x in range(len(jadwalSatu)):
+            self.insertRowSatu(jadwalSatu[x])           
+        
+        worksheetsDua = sheets.worksheet("MODUL 7")  
+        jadwalDua = worksheetsDua.get_all_values()
+        for x in range(len(jadwalDua)):
+            self.insertRowDua(jadwalDua[x])
+        
+        worksheetsTiga = sheets.worksheet("MODUL 8")       
+        jadwalTiga = worksheetsTiga.get_all_values()
+        for x in range(len(jadwalTiga)):
+            self.insertRowTiga(jadwalTiga[x])        
+        
 
         saveButton = self.tabWidget.findChild(QPushButton)
         infoBox = self.tabWidget.findChild(QTextEdit)
         data = infoBox.toPlainText()
         saveButton.clicked.connect(self.insertData)
 
-    def insertRow(self, items):
-        rowPosition = self.tableWidget.rowCount()
-        self.tableWidget.insertRow(rowPosition)
+    def insertRowSatu(self, items):
+        rowPosition = self.Modul_6.rowCount()
+        self.Modul_6.insertRow(rowPosition)
         for x in range(len(items)):
             qtablewidgetitem = QTableWidgetItem()
-            self.tableWidget.setItem(rowPosition, x, qtablewidgetitem)
-            qtablewidgetitem = self.tableWidget.item(rowPosition, x)
+            self.Modul_6.setItem(rowPosition, x, qtablewidgetitem)
+            qtablewidgetitem = self.Modul_6.item(rowPosition, x)
+            qtablewidgetitem.setText(items[x])
+            
+    def insertRowDua(self, items):
+        rowPosition = self.Modul_7.rowCount()
+        self.Modul_7.insertRow(rowPosition)
+        for x in range(len(items)):
+            qtablewidgetitem = QTableWidgetItem()
+            self.Modul_7.setItem(rowPosition, x, qtablewidgetitem)
+            qtablewidgetitem = self.Modul_7.item(rowPosition, x)
+            qtablewidgetitem.setText(items[x])        
+
+    def insertRowTiga(self, items):
+        rowPosition = self.Modul_8.rowCount()
+        self.Modul_8.insertRow(rowPosition)
+        for x in range(len(items)):
+            qtablewidgetitem = QTableWidgetItem()
+            self.Modul_8.setItem(rowPosition, x, qtablewidgetitem)
+            qtablewidgetitem = self.Modul_8.item(rowPosition, x)
             qtablewidgetitem.setText(items[x])
 
     def insertData(self):
