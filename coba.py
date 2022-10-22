652ce5b984b77c640bfc39206852fabb2271021b
Luqman <51292479+burjoy@users.noreply.github.com>
2022-10-22 19:22:51 +0700

    Update coba.py
    Bisa update, tapi kudu dipencet dulu tombol refreshnye

--- coba.py
+++ coba.py
@@ -14,32 +14,14 @@ class UI(QMainWindow):
         print(state)
         self.show()
 
-        open = gspread.service_account(filename="creds.json")
-        sheets = open.open_by_key(
-            "1gVmaW9uDWLIy7-B90HDruVZSlTZnTSGv_mGj-BmWRTo")
-        
-        tabWidget2 = self.findChild(QTableWidget, "Modul_7")
+        self.showData()
 
-        worksheetsSatu = sheets.worksheet("MODUL 6")
-        jadwalSatu = worksheetsSatu.get_all_values()
-        for x in range(len(jadwalSatu)):
-            self.insertRowSatu(jadwalSatu[x])           
-        
-        worksheetsDua = sheets.worksheet("MODUL 7")  
-        jadwalDua = worksheetsDua.get_all_values()
-        for x in range(len(jadwalDua)):
-            self.insertRowDua(jadwalDua[x])
-        
-        worksheetsTiga = sheets.worksheet("MODUL 8")       
-        jadwalTiga = worksheetsTiga.get_all_values()
-        for x in range(len(jadwalTiga)):
-            self.insertRowTiga(jadwalTiga[x])        
-        
-
+        refresh = self.findChild(QPushButton, "refresh")
         saveButton = self.tabWidget.findChild(QPushButton)
         infoBox = self.tabWidget.findChild(QTextEdit)
         data = infoBox.toPlainText()
         saveButton.clicked.connect(self.insertData)
+        refresh.clicked.connect(self.showData)
 
     def insertRowSatu(self, items):
         rowPosition = self.Modul_6.rowCount()
@@ -49,7 +31,7 @@ class UI(QMainWindow):
             self.Modul_6.setItem(rowPosition, x, qtablewidgetitem)
             qtablewidgetitem = self.Modul_6.item(rowPosition, x)
             qtablewidgetitem.setText(items[x])
-            
+
     def insertRowDua(self, items):
         rowPosition = self.Modul_7.rowCount()
         self.Modul_7.insertRow(rowPosition)
@@ -57,7 +39,7 @@ class UI(QMainWindow):
             qtablewidgetitem = QTableWidgetItem()
             self.Modul_7.setItem(rowPosition, x, qtablewidgetitem)
             qtablewidgetitem = self.Modul_7.item(rowPosition, x)
-            qtablewidgetitem.setText(items[x])        
+            qtablewidgetitem.setText(items[x])
 
     def insertRowTiga(self, items):
         rowPosition = self.Modul_8.rowCount()
@@ -69,13 +51,41 @@ class UI(QMainWindow):
             qtablewidgetitem.setText(items[x])
 
     def insertData(self):
-        open = gspread.service_account(filename="creds.json")
+        open = gspread.service_account(
+            filename="creds.json")
         sheets = open.open_by_key(
             "1gVmaW9uDWLIy7-B90HDruVZSlTZnTSGv_mGj-BmWRTo")
         worksheet = sheets.worksheet("MODUL 8")
         infoBox = self.tabWidget.findChild(QTextEdit)
         peserta = infoBox.toPlainText()
         worksheet.update_cell(2, 3, peserta)
+
+    def showData(self):
+        self.Modul_6.clear()
+        self.Modul_7.clear()
+        self.Modul_8.clear()
+
+        self.Modul_6.setRowCount(0)
+        self.Modul_7.setRowCount(0)
+        self.Modul_8.setRowCount(0)
+        open = gspread.service_account(filename="creds.json")
+        sheets = open.open_by_key(
+            "1gVmaW9uDWLIy7-B90HDruVZSlTZnTSGv_mGj-BmWRTo")
+
+        worksheetsSatu = sheets.worksheet("MODUL 6")
+        jadwalSatu = worksheetsSatu.get_all_values()
+        for x in range(len(jadwalSatu)):
+            self.insertRowSatu(jadwalSatu[x])
+
+        worksheetsDua = sheets.worksheet("MODUL 7")
+        jadwalDua = worksheetsDua.get_all_values()
+        for x in range(len(jadwalDua)):
+            self.insertRowDua(jadwalDua[x])
+
+        worksheetsTiga = sheets.worksheet("MODUL 8")
+        jadwalTiga = worksheetsTiga.get_all_values()
+        for x in range(len(jadwalTiga)):
+            self.insertRowTiga(jadwalTiga[x])
 
 
 if __name__ == "__main__":
