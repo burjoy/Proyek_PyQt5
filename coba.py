3c6afa2decd3f6e8b76e153e9a106ba7b8320c11
Luqman <51292479+burjoy@users.noreply.github.com>
2022-10-28 13:25:27 +0700

    Udah bisa input praktikan

--- coba.py
+++ coba.py
@@ -8,15 +8,16 @@ import numpy as np
 
 hitung = 0
 
+
 class UI(QMainWindow):
-    def __init__(self, state= False):
+    def __init__(self, state=False):
         super(UI, self).__init__()
         loadUi("Design_Trial.ui", self)
 
         self.deterstate(state)
         self.show()
         global hitung
-        
+
         open = gspread.service_account(filename="creds.json")
         sheets = open.open_by_key(
             "1gVmaW9uDWLIy7-B90HDruVZSlTZnTSGv_mGj-BmWRTo")
@@ -24,46 +25,47 @@ class UI(QMainWindow):
         worksheetsSatu = sheets.worksheet("MODUL 6")
         worksheetsDua = sheets.worksheet("MODUL 7")
         worksheetsTiga = sheets.worksheet("MODUL 8")
-        wsarr = np.array([worksheetsSatu,worksheetsDua,worksheetsTiga])
+        wsarr = np.array([worksheetsSatu, worksheetsDua, worksheetsTiga])
 
         self.showData(wsarr)
 
         clear = self.findChild(QPushButton, "clear")
-        clear.clicked.connect(lambda : self.clearData(self.tabWidget_2.currentIndex(),wsarr))
+        clear.clicked.connect(lambda: self.clearData(
+            self.tabWidget_2.currentIndex(), wsarr))
 
         saveButton = self.tabWidget.findChild(QPushButton)
-        saveButton.clicked.connect(lambda : self.saveJadwal(wsarr))
+        saveButton.clicked.connect(lambda: self.saveJadwal(wsarr))
 
         saveBtnAslab = self.findChild(QPushButton, "saveaslab")
-        saveBtnAslab.clicked.connect(lambda : self.saveJadwalAslab(wsarr))
-        #allval = 
+        saveBtnAslab.clicked.connect(lambda: self.saveJadwalAslab(wsarr))
+        # allval =
 
-        #counter
+        # counter
         nambah = self.findChild(QPushButton, "incButton")
         kurang = self.findChild(QPushButton, "decButton")
-        kelompok = self.findChild(QLabel,"layarHitung")
+        kelompok = self.findChild(QLabel, "layarHitung")
         kelompok.setNum(0)
 
         nambah.clicked.connect(self.counterUp)
         kurang.clicked.connect(self.counterDown)
-        
+
         simpan = self.findChild(QPushButton, "simpan")
-        simpan.clicked.connect(self.saveJadwal)
-    
-    def deterstate(self,state):
+        simpan.clicked.connect(lambda: self.saveJadwal(wsarr))
+
+    def deterstate(self, state):
         statelabel = self.findChild(QLabel, "label_5")
         statelabel.setStyleSheet("background-color: yellow; font: 12pt;")
-        if state == True : 
+        if state == True:
             statelabel.setText("Aslab")
             self.tabWidget.removeTab(0)
-        else : 
+        else:
             statelabel.setText("Praktikan")
             self.tabWidget.removeTab(1)
 
     def saveJadwal(self, sheetp):
-        
+
         modul6Sesi = self.findChild(QComboBox, "modul6_sesi")
-        modul6Tanggal = self.findChild(QComboBox, "modul6_tanggal")          
+        modul6Tanggal = self.findChild(QComboBox, "modul6_tanggal")
         modul7Sesi = self.findChild(QComboBox, "modul7_sesi")
         modul7Tanggal = self.findChild(QComboBox, "modul7_tanggal")
         modul8Sesi = self.findChild(QComboBox, "modul8_sesi")
@@ -72,11 +74,11 @@ class UI(QMainWindow):
         print('\n\nNo Kelompok :' + str(hitung))
         print('\nModul 6 :')
         print("Tanggal :" + modul6Tanggal.currentText())
-        print("Sesi ke-" + modul6Sesi.currentText()) 
+        print("Sesi ke-" + modul6Sesi.currentText())
 
         print('\nModul 7 :')
         print("Tanggal :" + modul7Tanggal.currentText())
-        print("Sesi ke-" + modul7Sesi.currentText())         
+        print("Sesi ke-" + modul7Sesi.currentText())
 
         print('\nModul 8 :')
         print("Tanggal :" + modul8Tanggal.currentText())
@@ -85,119 +87,121 @@ class UI(QMainWindow):
         #sesiPrak = np.array([int(modul6Sesi.currentText()), int(modul7Sesi.currentText()), int(modul8Sesi.currentText())])
         #hariPrak = np.array([modul6Tanggal.currentText(), modul7Tanggal.currentText(), modul8Tanggal.currentText()])
 
-        self.pilihModul6(sheetp[0], int(modul6Sesi.currentText()), modul6Tanggal.currentText())
-        #self.pilihModul7(sheetp[1], sesiPrak[1], hariPrak[1])
+        self.pilihModul6(sheetp[0], int(modul6Sesi.currentText()),
+                         modul6Tanggal.currentText())
+        self.pilihModul7(sheetp[1], int(modul7Sesi.currentText()),
+                         modul7Tanggal.currentText())
+        self.pilihModul7(sheetp[2], int(modul8Sesi.currentText()),
+                         modul8Tanggal.currentText())
 
         self.showData(sheetp)
-        
-       
+
     def pilihModul6(self, sheetP, sesiPraktikum, hariPraktikum):
 
-        if hariPraktikum == 'Senin (31/10)' :
+        if hariPraktikum == 'Senin (31/10)':
             hariP = 1
-            if sesiPraktikum == 1 :
-               sheetP.update_cell(3,2+hariP,hitung)
-            elif sesiPraktikum == 2 :
-                sheetP.update_cell(5,2+hariP,hitung)
-            elif sesiPraktikum == 3 :
-                sheetP.update_cell(7,2+hariP,hitung)
-            elif sesiPraktikum == 4 :
-                sheetP.update_cell(9,2+hariP,hitung)
-        elif hariPraktikum == 'Selasa (1/11)' :
+            if sesiPraktikum == 1:
+                sheetP.update_cell(3, 2+hariP, hitung)
+            elif sesiPraktikum == 2:
+                sheetP.update_cell(5, 2+hariP, hitung)
+            elif sesiPraktikum == 3:
+                sheetP.update_cell(7, 2+hariP, hitung)
+            elif sesiPraktikum == 4:
+                sheetP.update_cell(9, 2+hariP, hitung)
+        elif hariPraktikum == 'Selasa (1/11)':
             hariP = 2
-            if sesiPraktikum == 1 :
-               sheetP.update_cell(3,2+hariP,hitung)
-            elif sesiPraktikum == 2 :
-                sheetP.update_cell(5,2+hariP,hitung)
-            elif sesiPraktikum == 3 :
-                sheetP.update_cell(7,2+hariP,hitung)
-            elif sesiPraktikum == 4 :
-                sheetP.update_cell(9,2+hariP,hitung)            
-        elif hariPraktikum == 'Rabu (2/11)' :
+            if sesiPraktikum == 1:
+                sheetP.update_cell(3, 2+hariP, hitung)
+            elif sesiPraktikum == 2:
+                sheetP.update_cell(5, 2+hariP, hitung)
+            elif sesiPraktikum == 3:
+                sheetP.update_cell(7, 2+hariP, hitung)
+            elif sesiPraktikum == 4:
+                sheetP.update_cell(9, 2+hariP, hitung)
+        elif hariPraktikum == 'Rabu (2/11)':
             hariP = 3
-            if sesiPraktikum == 1 :
-               sheetP.update_cell(3,2+hariP,hitung)
-            elif sesiPraktikum == 2 :
-                sheetP.update_cell(5,2+hariP,hitung)
-            elif sesiPraktikum == 3 :
-                sheetP.update_cell(7,2+hariP,hitung)
-            elif sesiPraktikum == 4 :
-                sheetP.update_cell(9,2+hariP,hitung)
-        elif hariPraktikum == 'Kamis (3/11)' :
+            if sesiPraktikum == 1:
+                sheetP.update_cell(3, 2+hariP, hitung)
+            elif sesiPraktikum == 2:
+                sheetP.update_cell(5, 2+hariP, hitung)
+            elif sesiPraktikum == 3:
+                sheetP.update_cell(7, 2+hariP, hitung)
+            elif sesiPraktikum == 4:
+                sheetP.update_cell(9, 2+hariP, hitung)
+        elif hariPraktikum == 'Kamis (3/11)':
             hariP = 4
-            if sesiPraktikum == 1 :
-               sheetP.update_cell(3,2+hariP,hitung)
-            elif sesiPraktikum == 2 :
-                sheetP.update_cell(5,2+hariP,hitung)
-            elif sesiPraktikum == 3 :
-                sheetP.update_cell(7,2+hariP,hitung)
-            elif sesiPraktikum == 4 :
-                sheetP.update_cell(9,2+hariP,hitung)
-        elif hariPraktikum == 'Jumat (4/11)' :       
+            if sesiPraktikum == 1:
+                sheetP.update_cell(3, 2+hariP, hitung)
+            elif sesiPraktikum == 2:
+                sheetP.update_cell(5, 2+hariP, hitung)
+            elif sesiPraktikum == 3:
+                sheetP.update_cell(7, 2+hariP, hitung)
+            elif sesiPraktikum == 4:
+                sheetP.update_cell(9, 2+hariP, hitung)
+        elif hariPraktikum == 'Jumat (4/11)':
             hariP = 5
-            if sesiPraktikum == 1 :
-               sheetP.update_cell(3,2+hariP,hitung)
-            elif sesiPraktikum == 2 :
-                sheetP.update_cell(5,2+hariP,hitung)
-            elif sesiPraktikum == 3 :
-                sheetP.update_cell(7,2+hariP,hitung)
-            elif sesiPraktikum == 4 :
-                sheetP.update_cell(9,2+hariP,hitung)
+            if sesiPraktikum == 1:
+                sheetP.update_cell(3, 2+hariP, hitung)
+            elif sesiPraktikum == 2:
+                sheetP.update_cell(5, 2+hariP, hitung)
+            elif sesiPraktikum == 3:
+                sheetP.update_cell(7, 2+hariP, hitung)
+            elif sesiPraktikum == 4:
+                sheetP.update_cell(9, 2+hariP, hitung)
 
     def pilihModul7(self, sheetP, sesiPraktikum, hariPraktikum):
-        if hariPraktikum == 'Senin (7/11)' :
+        if hariPraktikum == 'Senin (7/11)':
             hariP = 1
-            if sesiPraktikum == 1 :
-               sheetP.update_cell(3,2+hariP,hitung)
-            elif sesiPraktikum == 2 :
-                sheetP.update_cell(5,2+hariP,hitung)
-            elif sesiPraktikum == 3 :
-                sheetP.update_cell(7,2+hariP,hitung)
-            elif sesiPraktikum == 4 :
-                sheetP.update_cell(9,2+hariP,hitung)
-        elif hariPraktikum == 'Selasa (8/11)' :
+            if sesiPraktikum == 1:
+                sheetP.update_cell(3, 2+hariP, hitung)
+            elif sesiPraktikum == 2:
+                sheetP.update_cell(5, 2+hariP, hitung)
+            elif sesiPraktikum == 3:
+                sheetP.update_cell(7, 2+hariP, hitung)
+            elif sesiPraktikum == 4:
+                sheetP.update_cell(9, 2+hariP, hitung)
+        elif hariPraktikum == 'Selasa (8/11)':
             hariP = 2
-            if sesiPraktikum == 1 :
-               sheetP.update_cell(3,2+hariP,hitung)
-            elif sesiPraktikum == 2 :
-                sheetP.update_cell(5,2+hariP,hitung)
-            elif sesiPraktikum == 3 :
-                sheetP.update_cell(7,2+hariP,hitung)
-            elif sesiPraktikum == 4 :
-                sheetP.update_cell(9,2+hariP,hitung)            
-        elif hariPraktikum == 'Rabu (9/11)' :
+            if sesiPraktikum == 1:
+                sheetP.update_cell(3, 2+hariP, hitung)
+            elif sesiPraktikum == 2:
+                sheetP.update_cell(5, 2+hariP, hitung)
+            elif sesiPraktikum == 3:
+                sheetP.update_cell(7, 2+hariP, hitung)
+            elif sesiPraktikum == 4:
+                sheetP.update_cell(9, 2+hariP, hitung)
+        elif hariPraktikum == 'Rabu (9/11)':
             hariP = 3
-            if sesiPraktikum == 1 :
-               sheetP.update_cell(3,2+hariP,hitung)
-            elif sesiPraktikum == 2 :
-                sheetP.update_cell(5,2+hariP,hitung)
-            elif sesiPraktikum == 3 :
-                sheetP.update_cell(7,2+hariP,hitung)
-            elif sesiPraktikum == 4 :
-                sheetP.update_cell(9,2+hariP,hitung)
-        elif hariPraktikum == 'Kamis (10/11)' :
+            if sesiPraktikum == 1:
+                sheetP.update_cell(3, 2+hariP, hitung)
+            elif sesiPraktikum == 2:
+                sheetP.update_cell(5, 2+hariP, hitung)
+            elif sesiPraktikum == 3:
+                sheetP.update_cell(7, 2+hariP, hitung)
+            elif sesiPraktikum == 4:
+                sheetP.update_cell(9, 2+hariP, hitung)
+        elif hariPraktikum == 'Kamis (10/11)':
             hariP = 4
-            if sesiPraktikum == 1 :
-               sheetP.update_cell(3,2+hariP,hitung)
-            elif sesiPraktikum == 2 :
-                sheetP.update_cell(5,2+hariP,hitung)
-            elif sesiPraktikum == 3 :
-                sheetP.update_cell(7,2+hariP,hitung)
-            elif sesiPraktikum == 4 :
-                sheetP.update_cell(9,2+hariP,hitung)
-        elif hariPraktikum == 'Jumat (11/11)' :     
+            if sesiPraktikum == 1:
+                sheetP.update_cell(3, 2+hariP, hitung)
+            elif sesiPraktikum == 2:
+                sheetP.update_cell(5, 2+hariP, hitung)
+            elif sesiPraktikum == 3:
+                sheetP.update_cell(7, 2+hariP, hitung)
+            elif sesiPraktikum == 4:
+                sheetP.update_cell(9, 2+hariP, hitung)
+        elif hariPraktikum == 'Jumat (11/11)':
             hariP = 5
-            if sesiPraktikum == 1 :
-               sheetP.update_cell(3,2+hariP,hitung)
-            elif sesiPraktikum == 2 :
-                sheetP.update_cell(5,2+hariP,hitung)
-            elif sesiPraktikum == 3 :
-                sheetP.update_cell(7,2+hariP,hitung)
-            elif sesiPraktikum == 4 :
-                sheetP.update_cell(9,2+hariP,hitung)
-    
-    
-    def saveJadwalAslab(self,sheetar):
+            if sesiPraktikum == 1:
+                sheetP.update_cell(3, 2+hariP, hitung)
+            elif sesiPraktikum == 2:
+                sheetP.update_cell(5, 2+hariP, hitung)
+            elif sesiPraktikum == 3:
+                sheetP.update_cell(7, 2+hariP, hitung)
+            elif sesiPraktikum == 4:
+                sheetP.update_cell(9, 2+hariP, hitung)
+
+    def saveJadwalAslab(self, sheetar):
         kodeAslab = self.findChild(QLineEdit, "kodeaslab")
         modulAslab = self.findChild(QComboBox, "modulaslab")
         hariAslab = self.findChild(QComboBox, "hariaslab")
@@ -205,61 +209,64 @@ class UI(QMainWindow):
         sesi2aslab = self.findChild(QCheckBox, "sesi2aslab")
         sesi3aslab = self.findChild(QCheckBox, "sesi3aslab")
         sesi4aslab = self.findChild(QCheckBox, "sesi4aslab")
-        sesiArray = np.array([sesi1aslab,sesi2aslab,sesi3aslab,sesi4aslab])
-        sesiaslab = np.array([sesi1aslab.isChecked(),sesi2aslab.isChecked(),sesi3aslab.isChecked(),sesi4aslab.isChecked()])
+        sesiArray = np.array([sesi1aslab, sesi2aslab, sesi3aslab, sesi4aslab])
+        sesiaslab = np.array([sesi1aslab.isChecked(), sesi2aslab.isChecked(
+        ), sesi3aslab.isChecked(), sesi4aslab.isChecked()])
         #print(kodeAslab.text(), modulAslab.currentText(), hariAslab.currentText())
-        #print(sesiaslab)
+        # print(sesiaslab)
 
-        if kodeAslab.text() == '' :
+        if kodeAslab.text() == '':
             self.error("Tidak ada Kode Aslab")
             return
-        else :
-            if modulAslab.currentText() == "Modul 6" :
-                self.aslabUpdate(sheetar[0],hariAslab.currentText(),sesiaslab,kodeAslab.text())
-            elif modulAslab.currentText() == "Modul 7" :
-                self.aslabUpdate(sheetar[1],hariAslab.currentText(),sesiaslab,kodeAslab.text())
-            elif modulAslab.currentText() == "Modul 8" :
-                self.aslabUpdate(sheetar[2],hariAslab.currentText(),sesiaslab,kodeAslab.text())
-            
-            self.aslabclear(hariAslab,modulAslab,sesiArray)
-        
-        self.showData(sheetar)
-        
+        else:
+            if modulAslab.currentText() == "Modul 6":
+                self.aslabUpdate(
+                    sheetar[0], hariAslab.currentText(), sesiaslab, kodeAslab.text())
+            elif modulAslab.currentText() == "Modul 7":
+                self.aslabUpdate(
+                    sheetar[1], hariAslab.currentText(), sesiaslab, kodeAslab.text())
+            elif modulAslab.currentText() == "Modul 8":
+                self.aslabUpdate(
+                    sheetar[2], hariAslab.currentText(), sesiaslab, kodeAslab.text())
 
-    def aslabUpdate(self,sheet,hari,sesiarr,kode):
+            self.aslabclear(hariAslab, modulAslab, sesiArray)
+
+        self.showData(sheetar)
+
+    def aslabUpdate(self, sheet, hari, sesiarr, kode):
         if hari == 'Senin':
             inthari = 1
             for i in range(len(sesiarr)):
                 if sesiarr[i] == True:
-                    sheet.update_cell(2+2*i,2+inthari,kode)
+                    sheet.update_cell(2+2*i, 2+inthari, kode)
 
         elif hari == 'Selasa':
             inthari = 2
             for i in range(len(sesiarr)):
                 if sesiarr[i] == True:
-                    sheet.update_cell(2+2*i,2+inthari,kode)
+                    sheet.update_cell(2+2*i, 2+inthari, kode)
 
         elif hari == 'Rabu':
             inthari = 3
             for i in range(len(sesiarr)):
                 if sesiarr[i] == True:
-                    sheet.update_cell(2+2*i,2+inthari,kode)
+                    sheet.update_cell(2+2*i, 2+inthari, kode)
 
         elif hari == 'Kamis':
             inthari = 4
             for i in range(len(sesiarr)):
                 if sesiarr[i] == True:
-                    sheet.update_cell(2+2*i,2+inthari,kode)
+                    sheet.update_cell(2+2*i, 2+inthari, kode)
 
         elif hari == 'Jumat':
             inthari = 5
             for i in range(len(sesiarr)):
                 if sesiarr[i] == True:
-                    sheet.update_cell(2+2*i,2+inthari,kode)
+                    sheet.update_cell(2+2*i, 2+inthari, kode)
 
-    def aslabclear(self,hariAslab,modulAslab,sesiarr):
+    def aslabclear(self, hariAslab, modulAslab, sesiarr):
         hariAslab.setCurrentIndex(hariAslab.currentIndex()+1)
-        if hariAslab.currentIndex() == -1 :
+        if hariAslab.currentIndex() == -1:
             modulAslab.setCurrentIndex(modulAslab.currentIndex()+1)
             hariAslab.setCurrentIndex(0)
         sesiarr[0].setCheckState(False)
@@ -270,13 +277,13 @@ class UI(QMainWindow):
     def counterUp(self):
         global hitung
         hitung += 1
-        kelompok = self.findChild(QLabel,"layarHitung")
+        kelompok = self.findChild(QLabel, "layarHitung")
         kelompok.setNum(hitung)
 
     def counterDown(self):
         global hitung
         hitung -= 1
-        kelompok = self.findChild(QLabel,"layarHitung")
+        kelompok = self.findChild(QLabel, "layarHitung")
         kelompok.setNum(hitung)
 
     def insertRowSatu(self, items):
@@ -306,8 +313,7 @@ class UI(QMainWindow):
             qtablewidgetitem = self.Modul_8.item(rowPosition, x)
             qtablewidgetitem.setText(items[x])
 
-
-    def showData(self,sheetarr):
+    def showData(self, sheetarr):
         self.Modul_6.clear()
         self.Modul_7.clear()
         self.Modul_8.clear()
@@ -315,7 +321,7 @@ class UI(QMainWindow):
         self.Modul_6.setRowCount(0)
         self.Modul_7.setRowCount(0)
         self.Modul_8.setRowCount(0)
-        
+
         jadwalSatu = sheetarr[0].get_all_values()
         for x in range(len(jadwalSatu)):
             self.insertRowSatu(jadwalSatu[x])
@@ -328,17 +334,18 @@ class UI(QMainWindow):
         for x in range(len(jadwalTiga)):
             self.insertRowTiga(jadwalTiga[x])
 
-    def clearData(self,tabnum,wsarr):
+    def clearData(self, tabnum, wsarr):
         wsarr[tabnum].batch_clear(["C2:G9"])
         self.showData(wsarr)
 
-    def error(self,errormsg):
+    def error(self, errormsg):
         msg = QMessageBox()
         msg.setIcon(QMessageBox.Critical)
         msg.setText("Error")
         msg.setInformativeText(errormsg)
         msg.setWindowTitle("Error")
         msg.exec_()
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
